c5be3c241aa343205613b07db7648c5e8c631412
vernistage <sdickers10@gmail.com>
2016-12-14 10:58:54 -0600

    add Babel as compiler

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-
+const ProductList = React.createClass({
+  render: function() {
+    return (
+      <div className='ui items'>
+        Hello, friend! I am a basic React component.
+      </div>
+    );
+  },
+});
