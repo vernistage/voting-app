9664c73b606eceeae46c38640d01fb2ee0433802
vernistage <sdickers10@gmail.com>
2016-12-14 12:02:38 -0600

    add product child to productlist parent

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const ProductList = React.createClass({
   render: function() {
     return (
       <div className='ui items'>
-        Hello, friend! I am a basic React component.
+        <Product />
       </div>
     );
   },
