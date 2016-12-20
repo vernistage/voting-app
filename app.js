9fca07934efa32038836dd65cb1c009be5f7c974
vernistage <sdickers10@gmail.com>
2016-12-20 14:45:36 -0600

    add downvote feature

--- app.js
+++ app.js
@@ -22,6 +22,15 @@ const ProductList = React.createClass({
     });
     this.updateState();
   },
+  handleProductDownVote: function (productId) {
+    Data.forEach((el) => {
+      if (el.id === productId) {
+        el.votes = el.votes - 1;
+        return;
+      }
+    });
+    this.updateState();
+  },
   render: function() {
     const products = this.state.products.map((product) => {
       return (
@@ -34,7 +43,8 @@ const ProductList = React.createClass({
           votes={product.votes}
           submitter_avatar_url={product.submitter_avatar_url}
           product_image_url={product.product_image_url}
-          onVote={this.handleProductUpVote}
+          onVoteUp={this.handleProductUpVote}
+          onVoteDown={this.handleProductDownVote}
         />
       );
     });
@@ -48,7 +58,10 @@ const ProductList = React.createClass({
 
 const Product = React.createClass({
   handleUpVote: function () {
-    this.props.onVote(this.props.id);
+    this.props.onVoteUp(this.props.id);
+  },
+  handleDownVote: function () {
+    this.props.onVoteDown(this.props.id);
   },
   render: function() {
     return (
@@ -61,7 +74,10 @@ const Product = React.createClass({
             <a onClick={this.handleUpVote}>
               <i className='large caret up icon'></i>
             </a>
-            {this.props.votes}
+              {this.props.votes}
+            <a onClick={this.handleDownVote}>
+              <i className='large caret down icon'></i>
+            </a>
           </div>
           <div className='description'>
             <a href={this.props.url}>
